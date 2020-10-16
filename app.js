a094df742964c2f584ced7157c06f7e86ff8e8d0
JoungSik <tjstlr2010@gmail.com>
2020-10-16 23:22:18 +0900

    [Add] dotenv & discord.js example

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-var express = require('express');
+require("dotenv").config();
+
+var express = require("express");
 var app = express();
-var client_id = '';
-var client_secret = '';
-var query = "번역할 문장을 입력하세요.";
-app.get('/translate', function (req, res) {
-   var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-   var request = require('request');
-   var options = {
-       url: api_url,
-       form: {'source':'ko', 'target':'en', 'text':query},
-       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-    };
-   request.post(options, function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-       res.end(body);
-     } else {
-       res.status(response.statusCode).end();
-       console.log('error = ' + response.statusCode);
-     }
-   });
- });
- app.listen(3000, function () {
-   console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
- });
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("message", (message) => {
+  console.log(message.content);
+  if (message.content === "!ping") {
+    message.channel.send("Pong.");
+  }
+});
+
+client.login(process.env.TOKEN);
+
+// var client_id = "";
+// var client_secret = "";
+// var query = "번역할 문장을 입력하세요.";
+
+// app.get("/translate", function (req, res) {
+//   var api_url = "https://openapi.naver.com/v1/papago/n2mt";
+//   var request = require("request");
+//   var options = {
+//     url: api_url,
+//     form: { source: "ko", target: "en", text: query },
+//     headers: {
+//       "X-Naver-Client-Id": client_id,
+//       "X-Naver-Client-Secret": client_secret,
+//     },
+//   };
+//   request.post(options, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+//       res.end(body);
+//     } else {
+//       res.status(response.statusCode).end();
+//       console.log("error = " + response.statusCode);
+//     }
+//   });
+// });
+
+// app.listen(3000, function () {
+//   console.log("http://127.0.0.1:3000/translate app listening on port 3000!");
+// });
