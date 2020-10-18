73030ddda504e4fa2b72a32a9d61051d646a4b19
JoungSik <tjstlr2010@gmail.com>
2020-10-18 11:34:31 +0900

    [Fix] 멘션은 같이 번역 안되도록 수정

---
+++ README.md
@@ -0,0 +1,17 @@
+![GitHub](https://img.shields.io/github/license/JoungSik/NIJIKR-Translator)
+![GitHub contributors](https://img.shields.io/github/contributors/JoungSik/NIJIKR-Translator)
+![GitHub last commit](https://img.shields.io/github/last-commit/JoungSik/NIJIKR-Translator)
+![GitHub forks](https://img.shields.io/github/forks/JoungSik/NIJIKR-Translator?style=social)
+![GitHub stars](https://img.shields.io/github/stars/JoungSik/NIJIKR-Translator?style=social)
+
+# Discord 번역 봇
+
+# 로컬 환경
+
+.env 파일 생성 후 내용 작성
+
+```.env
+TOKEN=
+NAVER_CLIENT_ID=
+NAVER_CLIENT_SECRET=
+```
--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const getLangCode = ({ query }) =>
   axios.post(
     "https://openapi.naver.com/v1/papago/detectLangs",
     {
-      query: query
+      query: query,
     },
     {
       headers: {
@@ -35,25 +35,12 @@ const getLangCode = ({ query }) =>
     }
   );
 
-
-
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
 
 client.once("ready", () => {
   console.log("Ready!");
-});
-
-client.on("message", (message) => {
-  // console.log(message.content);
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
-  }
-
-  if (message.react === ":flag_kr:") {
-    message.channel.send("KOR");
-  }
 });
 
 client.on("messageReactionAdd", async (reaction, user) => {
@@ -65,105 +52,81 @@ client.on("messageReactionAdd", async (reaction, user) => {
       return;
     }
   }
+
+  // 한국어
   if (emojiUnicode(reaction.emoji.name) === "1f1f0 1f1f7") {
-    // 한국어
-    // reaction.message.channel.send("KOR");
-    var query = reaction.message.content;
-    getLangCode({query: query})
+    const query = reaction.message.content.replace(/\@\S+ /g, "");
+    getLangCode({ query: query })
       .then((response) => {
-        console.log(response.data.langCode);
-        translate({ source: response.data.langCode, target: "ko", query: query })
-        .then((response) => {
-          reaction.message.channel.send(query);
-          reaction.message.channel.send(">>> " + response.data.message.result.translatedText);
+        translate({
+          source: response.data.langCode,
+          target: "ko",
+          query: query,
         })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log(err);
-    })
+          .then((response) => {
+            reaction.message.channel.send(query);
+            reaction.message.channel.send(
+              ">>> " + response.data.message.result.translatedText
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
+  // 일본어
   if (emojiUnicode(reaction.emoji.name) === "1f1ef 1f1f5") {
-    //일본어
-    var query = reaction.message.content;
-    getLangCode({query: query})
+    const query = reaction.message.content.replace(/\@\S+ /g, "");
+    getLangCode({ query: query })
       .then((response) => {
-        console.log(response.data.langCode);
-        translate({ source: response.data.langCode, target: "ja", query: query })
-        .then((response) => {
-          reaction.message.channel.send(query);
-          reaction.message.channel.send(">>> " + response.data.message.result.translatedText);
+        translate({
+          source: response.data.langCode,
+          target: "ja",
+          query: query,
         })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log(err);
-    })
+          .then((response) => {
+            reaction.message.channel.send(query);
+            reaction.message.channel.send(
+              ">>> " + response.data.message.result.translatedText
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
+  // 영어
   if (emojiUnicode(reaction.emoji.name) === "1f1fa 1f1f8") {
-    //영어
-    var query = reaction.message.content;
-    getLangCode({query: query})
+    const query = reaction.message.content.replace(/\@\S+ /g, "");
+    getLangCode({ query: query })
       .then((response) => {
-        console.log(response.data.langCode);
-        translate({ source: response.data.langCode, target: "en", query: query })
-        .then((response) => {
-          reaction.message.channel.send(query);
-          reaction.message.channel.send(">>> " + response.data.message.result.translatedText);
+        translate({
+          source: response.data.langCode,
+          target: "en",
+          query: query,
         })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log(err);
-    })
+          .then((response) => {
+            reaction.message.channel.send(query);
+            reaction.message.channel.send(
+              ">>> " + response.data.message.result.translatedText
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-  //console.log(`${reaction.codePointAt(0).toString(16)}'`);
-  //console.log(`${reaction.emoji.toString()}'`);
-  //console.log(emojiUnicode("📻"));
-  // console.log(emojiUnicode(reaction.emoji.name));
-  // console.log(
-  //   `${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`
-  // );
-  // console.log(
-  //   `${reaction.count} user(s) have given the same reaction to this message!`
-  // );
 });
 
 client.login(process.env.TOKEN);
-
-// var query = "번역할 문장을 입력하세요.";
-
-// app.get("/translate", function (req, res) {
-//   var api_url = "https://openapi.naver.com/v1/papago/n2mt";
-//   var request = require("request");
-//   var options = {
-//     url: api_url,
-//     form: { source: "ko", target: "en", text: query },
-//     headers: {
-//       "X-Naver-Client-Id": client_id,
-//       "X-Naver-Client-Secret": client_secret,
-//     },
-//   };
-//   request.post(options, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
-//       res.end(body);
-//     } else {
-//       res.status(response.statusCode).end();
-//       console.log("error = " + response.statusCode);
-//     }
-//   });
-// });
-
-// app.listen(3000, function () {
-//   console.log("http://127.0.0.1:3000/translate app listening on port 3000!");
-// });
